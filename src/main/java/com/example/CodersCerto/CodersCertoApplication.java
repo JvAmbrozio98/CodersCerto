f24a6ef9c4bc3a8b70588d8d9b15af03e54bcd1d
João Victor Ambrozio <jvambrozio98@gmail.com>
2024-07-02 18:15:37 -0300

    Initial commit

---
+++ src/main/java/com/example/CodersCerto/CodersCertoApplication.java
@@ -0,0 +1,13 @@
+package com.example.CodersCerto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodersCertoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodersCertoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CodersCerto/CodersCertoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CodersCerto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodersCertoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
